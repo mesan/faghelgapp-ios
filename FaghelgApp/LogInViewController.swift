7e52ca1d4d138054aa3ae1748a72470cf8dd0f3f
Anders Ullnæss <andersu@mesan.no>
2015-03-24 21:13:40 +0100

    Begynte å implementere Melding-siden

--- FaghelgApp/LogInViewController.swift
+++ FaghelgApp/LogInViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var textViewMessage: UITextView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var parentView: UIView!
+    
+    var constraintValue: CGFloat?
+    var placeholderText: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +31,20 @@ class LogInViewController: UIViewController {
                 self.registerForPush()
             }
         })
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        constraintValue = self.bottomConstraint.constant
+        placeholderText = textViewMessage.text
+        
+        textViewMessage.layer.borderColor = UIColor.lightGrayColor().CGColor
+        textViewMessage.layer.borderWidth = 1.0
+        textViewMessage.layer.cornerRadius = 8
+        textViewMessage.delegate = self
+        
+        var tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapReceived:")
+        self.view.addGestureRecognizer(tapGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,5 +61,41 @@ class LogInViewController: UIViewController {
         
         UIApplication.sharedApplication().registerUserNotificationSettings( settings )
     }
-
+    
+    func keyboardWillShow(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        if let height = tabBarController?.tabBar.frame.size.height {
+            self.bottomConstraint.constant = keyboardFrame.size.height - height + constraintValue!
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        var info = notification.userInfo!
+        
+        self.bottomConstraint.constant = constraintValue!
+    }
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textViewMessage.text == placeholderText {
+            textViewMessage.text = nil
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            textView.text = placeholderText
+        }
+        
+        // TODO: legg til meldingen i tabellen med klokkeslett og avsender
+        
+        return true
+    }
+    
+    func tapReceived(tapGestureRecognizer: UITapGestureRecognizer) {
+        if textViewMessage.isFirstResponder() && tapGestureRecognizer.view != textViewMessage {
+            textViewMessage.resignFirstResponder()
+        }
+    }
 }
